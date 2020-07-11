93ce47504386bfd4a2b7373b6e6e977e58375a20
Tanish Verma <tanishverma@tanish.local>
2020-07-11 19:43:29 +0530

    nlp linked

--- bot.py
+++ bot.py
@@ -19,8 +19,8 @@ raw=f.read()
 rawone=m.read()
 raw=raw.lower()# converts to lowercase
 rawone=rawone.lower()# converts to lowercase
-nltk.download('punkt') # first-time use only
-nltk.download('wordnet') # first-time use only
+#nltk.download('punkt') # first-time use only
+#nltk.download('wordnet') # first-time use only
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
 sent_tokensone = nltk.sent_tokenize(rawone)# converts to list of sentences 
@@ -39,16 +39,15 @@ def LemTokens(tokens):
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
-
-Introduce_Ans = ["My name is PyBot.","My name is PyBot you can called me pi.","Im PyBot :) ","My name is PyBot. and my nickname is pi and i am happy to solve your queries :) "]
+#Introduce_Ans = ["My name is EdyBot.","My name is EdyBot you can call me Edy.","Im EdyBot :) ","My name is EdyBot. and my nickname is Edy and i am happy to solve your queries :) "]
 GREETING_INPUTS = ("hello", "hi","hiii","hii","hiiii","hiiii", "greetings", "sup", "what's up","hey",)
-GREETING_RESPONSES = ["hi", "hey", "hii there", "hi there", "hello", "I am glad! You are talking to me"]
-Basic_Q = ("what is python ?","what is python","what is python?","what is python.")
-Basic_Ans = "Python is a high-level, interpreted, interactive and object-oriented scripting programming language python is designed to be highly readable It uses English keywords frequently where as other languages use punctuation, and it has fewer syntactical constructions than other languages."
-Basic_Om = ("what is module","what is module.","what is module ","what is module ?","what is module?","what is module in python","what is module in python.","what is module in python?","what is module in python ?")
-Basic_AnsM = ["Consider a module to be the same as a code library.","A file containing a set of functions you want to include in your application.","A module can define functions, classes and variables. A module can also include runnable code. Grouping related code into a module makes the code easier to understand and use."]
-
-
+GREETING_RESPONSES = ["Hi", "Hey", "Hi there", "hi there", "hello"]
+Basic_Q = ("what is NLP","what is Natural Language processing","what is Natural Language Processing?","tell me something about NLP")
+Basic_Ans = "Natural language processing (NLP) is a subfield of linguistics, computer science, information engineering, and artificial intelligence concerned with the interactions between computers and human (natural) languages, in particular how to program computers to process and analyze large amounts of natural language data."
+Basic_Om = ("who are you?" ,"what is your name" , "who are you")
+Basic_AnsM = ["My name is EdyBot.","My name is EdyBot you can call me Edy.","I am EdyBot :) ","My name is EdyBot. and my nickname is Edy and i am happy to solve your queries :) "]
+task = ("what can you do?","tell me something about yourself","what can you do")
+taskans = "Edy can help you in knowing about Natural Language Processing , Tell me what are you looking for today?"
 # Checking for greetings
 def greeting(sentence):
     """If user's input is a greeting, return a greeting response"""
@@ -62,22 +61,18 @@ def basic(sentence):
         if sentence.lower() == word:
             return Basic_Ans
 
+
 # Checking for Basic_QM
 def basicM(sentence):
-    """If user's input is a greeting, return a greeting response"""
     for word in Basic_Om:
         if sentence.lower() == word:
             return random.choice(Basic_AnsM)
-        
-# Checking for Introduce
-def IntroduceMe(sentence):
-    return random.choice(Introduce_Ans)
-
-
+    for word in task:
+        if sentence.lower() == word:
+            return taskans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-user_response = "bye"
 # Generating response
 def response(user_response):
     robo_response=''
@@ -91,11 +86,10 @@ def response(user_response):
     req_tfidf = flat[-2]
     if(req_tfidf==0):
         robo_response=robo_response+"I am sorry! I don't understand you"
-        return robo_response
+        return (robo_response)
     else:
         robo_response = robo_response+sent_tokens[idx]
-        return robo_response
-      
+        return (robo_response)    
 # Generating response
 def responseone(user_response):
     robo_response=''
@@ -109,44 +103,39 @@ def responseone(user_response):
     req_tfidf = flat[-2]
     if(req_tfidf==0):
         robo_response=robo_response+"I am sorry! I don't understand you"
-        return robo_response
+        return (robo_response)
     else:
         robo_response = robo_response+sent_tokensone[idx]
-        return robo_response
+        return (robo_response)
 
 
 def chat(user_response):
     user_response=user_response.lower()
-    keyword = " module "
+    keyword = "module "
     keywordone = " module"
-    keywordsecond = "module "
+    keywordsecond = " module "
     
     if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
+        if(user_response=='thanks' or user_response=='thank you' or user_response=='thanks edy' ):
             flag=False
-            print("ROBO: You are welcome..")
-            #return "You are welcome.."
-        elif(basicM(user_response)!=None):
-            return basicM(user_response)
+            return( "My Pleasure!")
+        
         else:
             if(user_response.find(keyword) != -1 or user_response.find(keywordone) != -1 or user_response.find(keywordsecond) != -1):
-                print("ROBO: ",end="")
-                print(responseone(user_response))
-                #return responseone(user_response)
+                return( responseone(user_response))
                 sent_tokensone.remove(user_response)
             elif(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
-                #return greeting(user_response)
-            elif(user_response.find("your name") != -1 or user_response.find(" your name") != -1 or user_response.find("your name ") != -1 or user_response.find(" your name ") != -1):
-                return IntroduceMe(user_response)
+                return( greeting(user_response))
+            
             elif(basic(user_response)!=None):
-                return basic(user_response)
+                return( basic(user_response))
+            elif(basicM(user_response)!=None):
+                return( basicM(user_response))  
             else:
-                print("ROBO: ",end="")
-                print(response(user_response))
-                #return response(user_response)
-                sent_tokens.remove(user_response)  
+                return( response(user_response))
+                sent_tokens.remove(user_response)
+                
     else:
         flag=False
-        print("ROBO: Bye! take care..")
-        #return "Bye! take care.."
+        return("Bye! take care..")
+chat("What is Python ?")
--- script.py
+++ script.py
@@ -3,12 +3,13 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer as cbt
 from chatterbot_corpus import *
 from webscrap import web_scraping
+from bot import chat
 
 app = Flask(__name__)
-eb = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = cbt(eb)
+#eb = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
+#trainer = cbt(eb)
 #trainer.train("chatterbot.corpus.english")
-trainer.train("data/data.yml")
+#trainer.train("data/data.yml")
 
 @app.route('/')  
 def home():  
@@ -17,10 +18,12 @@ def home():
 @app.route("/get")
 def get_resp():
     userText = request.args.get('msg')
-    if eb==Null
-    #ea = web_scraping(userText)
+    chat_output = chat(userText)
+    #return str(chat_output)
+    if chat_output=="I am sorry! I don't understand you":
+        return str(web_scraping(userText))
+    else:
+        return str(chat(userText))
     #return str(eb.get_response(userText))
-    return str(web_scraping(userText))
-
 if __name__ =='__main__':  
     app.run(debug = True)
