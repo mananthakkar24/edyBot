91f69ea75abe87a34f5ac5026094039378dec636
Tanish Verma <tanishverma@tanish.local>
2020-07-12 00:27:57 +0530

    nlp fixed

---
+++ advanceBOT.py
@@ -0,0 +1,86 @@
+from newspaper import Article
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import random
+import string
+import nltk
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+#nltk.download("punkt",quiet=True)
+
+article = Article("https://www.niituniversity.in/programmes/b-tech/computer-science-and-engineering")
+article.download()
+article.parse()
+article.nlp()
+corpus = article.text
+
+#print(corpus)
+
+text = corpus
+sentence_list = nltk.sent_tokenize(text)
+
+def greeting(text):
+    text=text.lower()
+    bot_greet = ["Hi","Hello","Hey","Namestey","Jai Shree Krishna"]
+    user_greet = ["Hi","Hello","Hey","hi","hello","hey"]
+    
+    for word in text.split():
+        if word in user_greet:
+            return random.choice(bot_greet)
+
+def index_sort(list_var):
+    length = len(list_var)
+    list_index = list(range(0,length))
+    
+    x = list_var
+    for i in range(length):
+        for j in range(length):
+            if x[list_index[i]] > x[list_index[j]]:
+                temp = list_index[i]
+                list_index[i] = list_index[j]
+                list_index[j] = temp
+    return list_index
+
+def bot_response(user_input):
+    user_input = user_input.lower()
+    sentence_list.append(user_input)
+    bot_response = ''
+    cm = CountVectorizer().fit_transform(sentence_list)
+    similarity_scores = cosine_similarity(cm[-1],cm)
+    similarity_scores_list = similarity_scores.flatten()
+    index = index_sort(similarity_scores_list)
+    index = index[1:]
+    response_flag = 0
+    
+    j = 0
+    for i in range(len(index)):
+        if similarity_scores_list[index[i]] > 0.0:
+            bot_response = bot_response+' '+sentence_list[index[i]]
+            response_flag = 1
+            j = j+1
+        if j > 2:
+            break
+    if response_flag == 0:
+        bot_response = bot_response+' '+"Sorry, Can't Find"
+        
+    sentence_list.remove(user_input)
+    
+    return bot_response
+
+exit_list = ['bye','thanks bye','quit','break','Bye']
+
+#while(True):
+def chat(userText):
+    user_input = userText
+    if user_input.lower() in exit_list:
+        print("Phir Miltey Hain, Alvida")
+        #break
+    else:
+        if greeting(user_input) != None:
+            #print("edyBot: ",greeting(user_input))
+            return greeting(user_input)
+        else:
+            #print("edyBot: ",bot_response(user_input))
+            return bot_response(user_input)
--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer as cbt
 from chatterbot_corpus import *
 from webscrap import web_scraping
-from bot import chat
+from advanceBOT import chat
 
 app = Flask(__name__)
 #eb = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
@@ -20,7 +20,7 @@ def get_resp():
     userText = request.args.get('msg')
     chat_output = chat(userText)
     #return str(chat_output)
-    if chat_output=="I am sorry! I don't understand you":
+    if chat_output==" Sorry, Can't Find":
         return str(web_scraping(userText))
     else:
         return str(chat(userText))
